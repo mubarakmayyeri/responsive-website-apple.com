8eca292d1d6866b01958b03e9f0c5b7cba75526a
Mubarak Mayyeri <mubarakmayyeri@gmail.com>
2022-09-23 08:38:13 +0530

    footer completed

---
+++ assets/js/footer.js
@@ -0,0 +1,25 @@
+//   Collapse  
+$('.panel-collapse').on('show.bs.collapse', function () {
+  $(this).siblings('.panel-title').addClass('active');
+});
+
+$('.panel-collapse').on('hide.bs.collapse', function () {
+  $(this).siblings('.panel-title').removeClass('active');
+});
+
+$('.panel-collapse2').on('show.bs.collapse', function () {
+  $(this).siblings('.panel-title').addClass('active');
+});
+
+$('.panel-collapse2').on('hide.bs.collapse', function () {
+  $(this).siblings('.panel-title').removeClass('active');
+});
+
+  $(window).on('resize', function(){
+  var windowSize = $(window).width();
+  if(windowSize < 720) {
+     $('.panel-collapse2').collapse('hide');
+  } else {
+      $('.panel-collapse2').collapse('show');
+  }
+});
